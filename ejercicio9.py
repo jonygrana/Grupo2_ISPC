37f1c23a2a9f7121bac6053a15b3af073cb040ad
eleonit <eleonit.services@gmail.com>
2022-08-13 17:45:50 -0300

    ejercicio9 - python

---
+++ ejercicio9.py
@@ -0,0 +1,10 @@
+# 9) Crear una tupla en Python con el nombre de “Historial5” la cual contenga los
+# siguientes valores: 8520, 9510, 7530, 3570, 1590
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Toto”.
+# Crear una función para determinar el valor máximo de atención gastada en “Toto”.
+
+historial5 = (8520, 9510, 7530, 3570, 1590)
+
+maximo = max(historial5)
+
+print("El monto máximo de atención gastada en concepto de servicios/consultas realizados a la mascota Toto es de $", maximo,".")
