9f970ca8f5a8c10e7cfd92a22bad21e8ee29b195
rociovolpato <rociovolpato@gmail.com>
2022-08-12 21:42:20 -0300

    agrego el archivo corregido

---
+++ introduccion_a_la_programacion.py
@@ -0,0 +1,12 @@
+DNI = 28957346
+nombre = "Juan"
+apellido = "Perez"
+telefono = 351443567
+direccion = "Belgrano 101"
+
+Dueno = [DNI, nombre, apellido, telefono, direccion]
+
+if DNI > 26000000:
+    print ("Telefono del dueño:", Dueno[3])
+
+
