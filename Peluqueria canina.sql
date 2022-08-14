665653bb56fe75ed8dbef4fc1d74d3f20b232f10
EmiliaGarz05 <106496375+EmiliaGarz05@users.noreply.github.com>
2022-08-14 17:13:39 -0300

    Update Peluqueria canina.sql
    limpie un poco el script porq lo hice con eer diagram

--- Peluqueria canina.sql
+++ Peluqueria canina.sql
@@ -1,13 +1,3 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema PeluqueriaHuellitas
--- -----------------------------------------------------
-
 -- -----------------------------------------------------
 -- Schema PeluqueriaHuellitas
 -- -----------------------------------------------------
@@ -24,7 +14,7 @@ CREATE TABLE IF NOT EXISTS `PeluqueriaHuellitas`.`Dueno` (
   `telefono_Du` BIGINT(30) NOT NULL,
   `direccion_Du` LONGTEXT NULL,
   PRIMARY KEY (`DNI_Du`))
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
@@ -43,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `PeluqueriaHuellitas`.`Perro` (
     REFERENCES `PeluqueriaHuellitas`.`Dueno` (`DNI_Du`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+;
 
 
 -- -----------------------------------------------------
@@ -62,9 +52,7 @@ CREATE TABLE IF NOT EXISTS `PeluqueriaHuellitas`.`Historial` (
     REFERENCES `PeluqueriaHuellitas`.`Perro` (`ID_Perro`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
