a786938cf1d0215108b4b4e455e4af8f08659705
EmiliaGarz05 <106496375+EmiliaGarz05@users.noreply.github.com>
2022-08-14 23:53:13 -0300

    Add files via upload

---
+++ ListaPERRO.py
@@ -0,0 +1,21 @@
+#Crear una lista denominada "Perro"
+
+ID_Perro= 13
+Nombre_Pe= "Puppy"
+FechaNac_Pe= "2012-12-12"
+Sexo_Pe= "macho"
+DNI_Dueno=123
+
+Perro=[ID_Perro,Nombre_Pe,FechaNac_Pe,Sexo_Pe,DNI_Dueno]
+
+#Mostrar lista Perro
+
+Print(Perro)
+
+#Modifico variable FechaNac_Pe y DNI_Du
+
+FechaNac_Pe="2012-12-13"
+DNI_Dueno=28957346
+
+Print(Perro)
+
